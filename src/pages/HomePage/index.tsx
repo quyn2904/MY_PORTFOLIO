90445823e9e22468b9465488866f8e16b9585e11
quyn2904 <quyenpq.work@gmail.com>
2024-10-22 22:34:32 +0700

    fix: button href to ssmcp

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,12 +1,9 @@
-import { useNavigate } from 'react-router-dom';
-
 const Homepage = (): JSX.Element => {
-  const nav = useNavigate();
   return (
     <div className="mx-auto mt-5 px-5">
       <button
         className="rounded-2xl bg-slate-500 px-8 py-5 text-5xl font-bold text-yellow-500"
-        onClick={() => nav('http://ssmcp.quyenpq.work')}
+        onClick={() => (window.location.href = 'http://ssmcp.quyenpq.work')}
       >
         SSMCP
       </button>
