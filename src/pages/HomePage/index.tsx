7bfb2546127443347c6b0de6f4626e118a90e3f6
quyn2904 <quyenpq.work@gmail.com>
2024-10-22 22:05:16 +0700

    react boilerplate with tailwindcss

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,11 @@
+const Homepage = (): JSX.Element => {
+  return (
+    <div className="mx-auto mt-5 px-5">
+      <button className="rounded-2xl bg-slate-500 px-8 py-5 text-5xl font-bold text-yellow-500">
+        SSMCP
+      </button>
+    </div>
+  );
+};
+
+export default Homepage;
---
+++ src/routes/index.tsx
@@ -0,0 +1,43 @@
+import {
+  createBrowserRouter,
+  Navigate,
+  Route,
+  RouteObject,
+  RouterProvider,
+  Routes,
+} from 'react-router-dom';
+import { HomePage, Login } from '../pages';
+
+const publicRoutes: RouteObject[] = [
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+];
+
+// const authenticatedRoutes: RouteType[] = [];
+
+const unauthenticatedRoutes: RouteObject[] = [
+  {
+    path: '/login',
+    element: <Login />,
+  },
+];
+
+const AppRouter = (): React.ReactElement => {
+  // const { token } = useAuth();
+
+  const router = createBrowserRouter([
+    ...publicRoutes,
+    ...unauthenticatedRoutes,
+    // ...(token ? authenticatedRoutes : unauthenticatedRoutes),
+    {
+      path: '*',
+      element: <Navigate to="/404" />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default AppRouter;
