6ad31d121395bb5a68e1a9967bb3a2da92574222
quyn2904 <quyenpq.work@gmail.com>
2024-10-22 22:23:56 +0700

    fix: combine to 1 workflow file

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,7 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+
 const Homepage = (): JSX.Element => {
+  const nav = useNavigate();
   return (
     <div className="mx-auto mt-5 px-5">
-      <button className="rounded-2xl bg-slate-500 px-8 py-5 text-5xl font-bold text-yellow-500">
+      <button
+        className="rounded-2xl bg-slate-500 px-8 py-5 text-5xl font-bold text-yellow-500"
+        onClick={() => nav('http://ssmcp.quyenpq.work/')}
+      >
         SSMCP
       </button>
     </div>
